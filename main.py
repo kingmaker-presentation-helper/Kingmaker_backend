9cdb390b931200ddc74e738c682d2e808b89678c
Jihae <62400658+easyssun@users.noreply.github.com>
2023-12-03 14:46:05 +0900

    포트 변경

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ app.add_middleware(
 
 if __name__ == "__main__":
 	import uvicorn
-	uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+	uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
