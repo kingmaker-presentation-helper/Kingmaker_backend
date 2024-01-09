2f1bdbc226f2818e19cc4530f64c6a67d3f640e9
ksj7385 <117991880+ksj7385@users.noreply.github.com>
2024-01-09 18:24:23 +0900

    제스처 단일이미지로 변경 및 threshhold 수정

--- func/pose.py
+++ func/pose.py
@@ -1,182 +1,88 @@
 import os
-import mediapipe as mp
 import cv2
+import mediapipe as mp
 import numpy as np
 import re
+
 def natural_sort_key(s):
     return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', s)]
 
-def calculate_angle(a,b,c):
-    
-    # 각 값을 받아 넘파이 배열로 변형
-    a = np.array(a) # 첫번째
-    b = np.array(b) # 두번째
-    c = np.array(c) # 세번째
-
-    # 라디안을 계산하고 실제 각도로 변경한다.
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
-
-    # 180도가 넘으면 360에서 뺀 값을 계산한다.
-    if angle >180.0:
-        angle = 360-angle
-
-    # 각도를 리턴한다.
+    if angle > 180.0:
+        angle = 360 - angle
     return angle
 
 async def detect_pose(session_key):
     local_file_path = f"user/{session_key}"
-    # ".png"나 '.jpg'로 끝나는 파일 불러오기
     img_list = [file for file in os.listdir(local_file_path) if file.endswith(".png") or file.endswith(".jpg")]
     img_list.sort(key=natural_sort_key)
-    
-    imgs = []
 
-    for img in img_list:
-        imgs.append(cv2.imread(local_file_path + '/' + img))
+    imgs = [cv2.imread(local_file_path + '/' + img) for img in img_list]
 
     mp_drawing = mp.solutions.drawing_utils
     mp_pose = mp.solutions.pose
 
-    # local_file_path에 capture 폴더 생성
     if not os.path.exists(local_file_path + '/capture'):
         os.makedirs(local_file_path + '/capture')
 
-    cap = cv2.VideoCapture(0)
-    count = 0
-    state1 = 0
-    state2 = 0
-    state3 = 0
-    state4 = 0
-    pose_state = 0
-
-    ## Setup mediapipe instance
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-        for frame in imgs:
-        # while cap.isOpened():
-            # ret, frame = cap.read()
-            # Recolor image to RGB
+        for idx, frame in enumerate(imgs):
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             image.flags.writeable = False
-        
-            # Make detection
+
             results = pose.process(image)
-        
-            # Recolor back to BGR
+
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            
-            # mkdir 
-            # Extract landmarks
+
             try:
                 landmarks = results.pose_landmarks.landmark
-                
-                # Get coordinates
+
                 left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
                 left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
                 left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-                
+
                 right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
                 right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
                 right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-                
-                # Calculate angle
+
                 left_angle1 = calculate_angle(right_shoulder, left_shoulder, left_elbow)
                 left_angle2 = calculate_angle(left_shoulder, left_elbow, left_wrist)
                 right_angle1 = calculate_angle(left_shoulder, right_shoulder, right_elbow)
                 right_angle2 = calculate_angle(right_shoulder, right_elbow, right_wrist)
-                
-                if left_angle1 < 115:
-                    state1 = 0
-                if left_angle1 > 120 and state1 == 0:
-                    state1= 1
 
-                if left_angle2 > 140:
-                    state2 = 0
-                if left_angle2 < 100 and state2 == 0:
-                    state2=1
-                
-                if right_angle1 < 115:
-                    state3 = 0
-                if right_angle1 > 120 and state3 == 0:
-                    state3= 1
-                
-                if right_angle2 > 140:
-                    state4 = 0
-                if right_angle2 < 100 and state4 == 0:
-                    state4=1
+                state1, state2, state3, state4 = 0, 0, 0, 0
 
-                if not pose_state:
-                    if state1 and state2:
-                        pose_state = 1
-                        count += 1
-                        cv2.imwrite(f'{local_file_path}/capture/pic' + str(count) +'.png', image)
-                    elif state1 and not state2:
-                        pose_state = 1
-                        count += 1
-                        cv2.imwrite(f'{local_file_path}/capture/pic' + str(count) +'.png', image)
-                    elif not state1 and state2:
-                        pose_state = 1
-                        count += 1
-                        cv2.imwrite(f'{local_file_path}/capture/pic' + str(count) +'.png', image)
+                if left_angle1 > 110:
+                    state1 = 1
+                if left_angle2 < 140:
+                    state2 = 1
+                if right_angle1 > 110:
+                    state3 = 1
+                if right_angle2 < 140:
+                    state4 = 1
 
-                    if state3 and state4:
-                        pose_state = 1
-                        count += 1
-                        cv2.imwrite(f'{local_file_path}/capture/pic' + str(count) +'.png', image)
-                    elif state3 and not state4:
-                        pose_state = 1
-                        count += 1
-                        cv2.imwrite(f'{local_file_path}/capture/pic' + str(count) +'.png', image)
-                    elif not state3 and state4:
-                        pose_state = 1
-                        count += 1
-                        cv2.imwrite(f'{local_file_path}/capture/pic' + str(count) +'.png', image)
+                if (state1 and state2) or (state3 and state4):
+                    cv2.imwrite(f'{local_file_path}/capture/pic' + str(idx) +'.png', image)
 
-                if (not state1 and not state2) and (not state3 and not state4) :
-                    pose_state = 0
+                print("Left Shoulder-Elbow angle: ", left_angle1, "Left Elbow-Wrist angle: ", left_angle2)
+                print("Right Shoulder-Elbow angle: ", right_angle1, "Right Elbow-Wrist angle: ", right_angle2)
 
-                
-                print("state1: ", state1, "state2: ", state2, "state3: ", state3, "state4: ", state4)
-                print("pose_state: ", pose_state)
-                
-                print(count)
-                    
-                # Visualize angle
-                cv2.putText(image, str(left_angle1), 
-                            tuple(np.multiply(left_shoulder, [640, 480]).astype(int)), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                    )
-                cv2.putText(image, str(left_angle2), 
-                            tuple(np.multiply(left_elbow, [640, 480]).astype(int)), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                    )
-                
-                cv2.putText(image, str(right_angle1), 
-                            tuple(np.multiply(right_shoulder, [640, 480]).astype(int)), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                    )
-                cv2.putText(image, str(right_angle2), 
-                            tuple(np.multiply(right_elbow, [640, 480]).astype(int)), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                    )
-                        
+                cv2.putText(image, str(left_angle1), tuple(np.multiply(left_shoulder, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.putText(image, str(left_angle2), tuple(np.multiply(left_elbow, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.putText(image, str(right_angle1), tuple(np.multiply(right_shoulder, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.putText(image, str(right_angle2), tuple(np.multiply(right_elbow, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
             except:
                 pass
-            
-            
-            # Render detections
+
             mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
-                                    )               
-            
-            # cv2.imshow('Mediapipe Feed', image)
-            # 0.1초 sleep
-            # cv2.waitKey(100)
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
 
-        #     if cv2.waitKey(10) & 0xFF == ord('q'):
-        #         break
 
-        # # cap.release()
-        # cv2.destroyAllWindows()
